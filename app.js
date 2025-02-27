3d6a39babe4aa2fc24a8c23315d39950cf2b566d
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-02-27 12:34:20 -0600

    Uso de carpeta estática para poder importar CSS y JS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express'); // Importa express
 const app = express(); // Llamar express de forma más rápida
 const bodyParser = require('body-parser'); // Importa body-parser
+const path = require('path'); // Permite usar direcciones del sistema
+
 
 // Permite leer el cuerpo de las solicitudes, es decir, permite leer los datos de un POST
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Le dice que va a usar la carpeta public como carpeta estática
+app.use(express.static(path.join(__dirname, 'public')));
 
 const mainRoutes = require('./routes/main.routes.js');
 app.use('/', mainRoutes);
