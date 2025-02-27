10c9209f2863ed4e1c3c9a9d3865b4256fdd6edd
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-02-26 22:42:07 -0600

    Laboratorio 10 terminado.

--- lab10/js/app.js
+++ lab10/js/app.js
@@ -1,42 +1,89 @@
 const file_system = require('fs');
 const http = require('http');
 
+const saveData = (arr) => {
+    let msg = "";
+    for(let i of arr){
+        msg += i + "\n";
+    }
+    file_system.writeFileSync('datos.txt', msg);
+}
+
 const getHTML = (adr) => {
     return file_system.readFileSync(('../lab10/html/'+adr+'.html'), {encoding: 'utf8', flag: 'r'});
 }
 
 const header = getHTML("header");
 const footer = getHTML("footer");
+const main = getHTML("main");
+const adder = getHTML("agregar");
+const extra = getHTML("extra");
+const about = getHTML("sobre");
+const error404 = getHTML("error");
 
-const getPage = (adr) => {
-    return header + getHTML(adr) + footer;
-}
+const people = []
+
+const stringPeople = (arr) => {
+    let msg = "";
+    for(let i of arr){
+        msg += "<p>" + i + "</p>";
+    }
+    return msg;
+};
 
 const server = http.createServer((request, response) =>{
-    
-    if (request.method == "GET" && (request.url == "/agregar" || request.url == "/")) {
-        console.log(request.url);
+
+    // Ruta base
+    if (request.method == "GET" && request.url == "/") { // El tener la url como / significa que no hay nada después
         response.setHeader('Content-Type', 'text/html');
-        response.write(getPage("main"));
+        response.write(header + main + footer);
+        response.end();
+    }
+    
+    // Ruta agregar
+    else if (request.method == "GET" && request.url == "/agregar") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write(header + adder + stringPeople(people) + footer);
         response.end();
     } else if (request.method == "POST" && request.url == "/agregar") {
 
-        const datos_completos = [];
+        const completeData = [];
 
         request.on('data', (data) => {
-            datos_completos.push(data);
-            console.log(data);
+            completeData.push(data);
         });
 
         request.on('end', () => {
-            const string_datos_completos = Buffer.concat(datos_completos).toString();
-            console.log(string_datos_completos);
-            const new_name = string_datos_completos.split('=')[1];
+            const stringCompleteData = decodeURIComponent(Buffer.concat(completeData).toString()).replace(/\+/g, " ");
+            const newName = stringCompleteData.split('=')[1];
+            people.push(newName);
+            saveData(people);
+
+            response.setHeader('Content-Type', 'text/html');
+            response.write(header + adder + stringPeople(people) + footer);
+            response.end();
         });
+    }
 
-    } else {
+    // Ruta about
+    else if (request.method == "GET" && request.url == "/about") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write(header + about + footer);
+        response.end();
+    }
+
+    // Ruta extra
+    else if (request.method == "GET" && request.url == "/extra") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write(header + extra + footer);
+        response.end();
+    }
+
+    // Ruta de error 404
+    else {
         response.statusCode = 404;
-
+        response.setHeader('Content-Type', 'text/html');
+        response.write(header + error404 + footer);
         response.end();
     }
 
